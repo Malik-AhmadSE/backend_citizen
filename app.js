7c6a8723514f05cae8652501b0d1f6c499a2ff9e
ahmad <ahmadali009ali008@gmail.com>
2023-07-17 14:51:06 +0500

    cors body parser

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express=require('express');
 const errorHandler = require('./middlewares/errorHandler');
 const app=express();
+const bodyparser =require("body-parser")
 const config=require('./config/index');
 const connectdb=require('./db/connectdb');
 const cookieParser = require('cookie-parser');
 const cors=require('cors');
+app.use(bodyparser.json())
 const port =config.PORT;
 const host=config.HOST;
 const DataBase=config.DATABASE_URL;
 const routes = require('./routes/routes');
-app.use(cors());
+const bodyParser = require('body-parser');
+app.use(
+    require("cors")({
+      origin: "*"
+    })
+  );
 app.use(cookieParser());
 app.use(express.static(__dirname + "/public/files"));
 app.use(express.static("public"));
