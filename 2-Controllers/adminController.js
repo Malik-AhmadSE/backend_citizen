37c3dd011e221de2703736bc719ba225d3f297f8
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2023-09-22 19:39:20 +0500

    add promice

--- 2-Controllers/adminController.js
+++ 2-Controllers/adminController.js
@@ -20,15 +20,16 @@ const AdminController = {
         result += totalprice[i];
       }
 
+      
       const UserChart= await getUserMap()
       const OrderChart= await getOrderMap()
       console.log("UserChart ",UserChart);
       const data = {
-        // products: products,
-        // user: user,
-        // sale: sale,
-        // totalearn: result,
-        // favorite: favorite,
+        products: products,
+        user: user,
+        sale: sale,
+        totalearn: result,
+        favorite: favorite,
         UserChart,
         OrderChart
       }
@@ -43,109 +44,105 @@ const AdminController = {
 };
 
 
-const getUserMap=async()=>{
+const getUserMap = async () => {
   const thirtyDaysAgo = new Date();
   thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
-  
-  let userCountsLast30Days; // Define a variable to store the result
-  
-  // Group users by registration date and count them, but only for the last 30 days
-  User.aggregate([
-    {
-      $match: {
-        createdAt: { $gte: thirtyDaysAgo }, // Filter users whose registration date is greater than or equal to 30 days ago
-      },
-    },
-    {
-      $group: {
-        _id: {
-          year: { $year: '$createdAt' },
-          month: { $month: '$createdAt' },
-          day: { $dayOfMonth: '$createdAt' },
-        },
-        count: { $sum: 1 },
-      },
-    },
-    {
-      $project: {
-        _id: 0,
-        date: {
-          $dateFromParts: {
-            year: '$_id.year',
-            month: '$_id.month',
-            day: '$_id.day',
-          },
-        },
-        count: 1,
-      },
-    },
-    {
-      $sort: { date: 1 },
-    },
-  ])
-    .then((result) => {
-      userCountsLast30Days = result; // Save the result in the variable
-      console.log("user ",userCountsLast30Days);
-      return userCountsLast30Days
-      // Now, userCountsLast30Days contains the count of users registered in the last 30 days
-    })
-    .catch((err) => {
-      console.error(err);
-      // Handle the error
-    });
-}
 
-const getOrderMap=async()=>{
+  // Use a promise to handle the asynchronous aggregation
+  return new Promise((resolve, reject) => {
+    User.aggregate([
+      {
+        $match: {
+          createdAt: { $gte: thirtyDaysAgo },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            year: { $year: '$createdAt' },
+            month: { $month: '$createdAt' },
+            day: { $dayOfMonth: '$createdAt' },
+          },
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          date: {
+            $dateFromParts: {
+              year: '$_id.year',
+              month: '$_id.month',
+              day: '$_id.day',
+            },
+          },
+          count: 1,
+        },
+      },
+      {
+        $sort: { date: 1 },
+      },
+    ])
+      .then((result) => {
+        resolve(result);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+const getOrderMap = async () => {
   const thirtyDaysAgo = new Date();
   thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
-  
-  let orderCountsLast30Days; // Define a variable to store the result
-  
-  // Group users by registration date and count them, but only for the last 30 days
-  Sales.aggregate([
-    {
-      $match: {
-        createdAt: { $gte: thirtyDaysAgo }, // Filter users whose registration date is greater than or equal to 30 days ago
-      },
-    },
-    {
-      $group: {
-        _id: {
-          year: { $year: '$createdAt' },
-          month: { $month: '$createdAt' },
-          day: { $dayOfMonth: '$createdAt' },
+
+  // Use a promise to handle the asynchronous aggregation
+  return new Promise((resolve, reject) => {
+    Sales.aggregate([
+      {
+        $match: {
+          createdAt: { $gte: thirtyDaysAgo },
         },
-        count: { $sum: 1 },
       },
-    },
-    {
-      $project: {
-        _id: 0,
-        date: {
-          $dateFromParts: {
-            year: '$_id.year',
-            month: '$_id.month',
-            day: '$_id.day',
+      {
+        $group: {
+          _id: {
+            year: { $year: '$createdAt' },
+            month: { $month: '$createdAt' },
+            day: { $dayOfMonth: '$createdAt' },
           },
+          count: { $sum: 1 },
         },
-        count: 1,
       },
-    },
-    {
-      $sort: { date: 1 },
-    },
-  ])
-    .then((result) => {
-      orderCountsLast30Days = result; // Save the result in the variable
-     // console.log(orderCountsLast30Days);
-      return orderCountsLast30Days
-      // Now, userCountsLast30Days contains the count of users registered in the last 30 days
-    })
-    .catch((err) => {
-      console.error(err);
-      // Handle the error
-    });
-}
+      {
+        $project: {
+          _id: 0,
+          date: {
+            $dateFromParts: {
+              year: '$_id.year',
+              month: '$_id.month',
+              day: '$_id.day',
+            },
+          },
+          count: 1,
+        },
+      },
+      {
+        $sort: { date: 1 },
+      },
+    ])
+      .then((result) => {
+        resolve(result);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+// Now, you can use these functions with async/await as before.
+
+
 
 
 module.exports = AdminController;
