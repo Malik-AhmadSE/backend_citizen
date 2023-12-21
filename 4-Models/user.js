d8699326a7975f95c22a4ec506d6e709ec3668ac
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2023-12-21 23:25:27 +0500

    update url

--- 4-Models/user.js
+++ 4-Models/user.js
@@ -4,7 +4,7 @@ const userSchema=mongoose.Schema({
     userName :{type:String,required:true},
     userEmail:{type:String,required:true},
     userPassword:{type:String,required:true},
-    userImage:{type:String,default:'http://localhost:8000/files/user.png'},
+    userImage:{type:String,default:'https://bcd.citizenblades.com/files/user.png'},
     Status:{type:String,default:'customer'},
 },
     {timestamps:true}
